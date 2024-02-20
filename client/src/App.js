9d99b4f306f89d7a33e95f18a7ba8ec246c42fed
Nuiosse11421 <notice.gameacc@gmail.com>
2024-02-20 22:50:16 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Home';
-import FormPage from './FormPage'; // Import your form page component
+import logo from './logo.svg';
+import './App.css';
+
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/form" component={FormPage} /> {/* Add this line */}
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 

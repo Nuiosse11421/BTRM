5f4c0a50b68fd8bf64735568e90382b57dfa1a52
Nuiosse11421 <notice.gameacc@gmail.com>
2024-02-20 22:39:26 +0700

    update index.js from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import FormPage from './FormPage'; // Import your form page component
 
 function App() {
   return (
-    <div>
-      <h1>Hello from App.js</h1>
-    </div>
-  )
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/form" component={FormPage} /> {/* Add this line */}
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/FormPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function FormPage() {
+  return (
+    <div>
+      <h1>Form Page</h1>
+      <form>
+        {/* Add your form fields here */}
+      </form>
+    </div>
+  );
+}
+
+export default FormPage;

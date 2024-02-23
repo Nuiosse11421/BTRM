5d6ced90566e445c86d94dc8c48947b9d312d237
Nuiosse11421 <notice.gameacc@gmail.com>
2024-02-23 22:19:36 +0700

    ทำให้หน้าสามารถ login ได้

--- client/src/LoginSignup.jsx
+++ client/src/LoginSignup.jsx
@@ -1,32 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Alert from './components/Alert'; // Import the Alert component
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import './TestLogin.js';
 import './LoginSignup.css'; // แก้ไขชื่อไฟล์ CSS
+
 function Anchor({ href, children }) {
     return <a href={href}>{children}</a>;
 }
 
-function LoginSignup() {
+const LoginSignup = () => {
+    const [formData, getFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        firstname: '',
+        lastname: '',
+        gender: '',
+        date_of_birth: '',
+    });
+    const dataChange = (element) => {
+        const { name, value } = element.target;
+        getFormData(prevState => ({ ...prevState, [name]: value }));
+    }
+
+    const SubmitRegister = async (element) => {
+        element.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:8000/register/api/createUser', formData);
+            console.log("User register successful", response.data);
+            setIsRegistered(true); // Set registration success state to true
+        } catch (err) {
+            console.error(err);
+            if (err.response && err.response.status === 400) {
+                setAlertMessage("User with the same email or username already exists");
+            }
+        }
+    }
+
     return (
-        <div className="Container" id='Container'>
+        <div className='Container' id='Container'> {/* Add active class if registration is successful */}
+            {/* Render alert if alertMessage is not null */}
+            {alertMessage && <Alert message={alertMessage} />}
             <div className='Form-container sign-up'>
-                <form>
+                <form onSubmit={SubmitRegister}>
                     <h1>Create Account</h1>
                     <div className="Social-icons">
-                        <FontAwesomeIcon icon={faGoogle} className="icon"/>
-                        <FontAwesomeIcon icon={faGithub} className="icon"/>
-                        <FontAwesomeIcon icon={faFacebook} className="icon"/>
+                        <FontAwesomeIcon icon={faGoogle} className="icon" />
+                        <FontAwesomeIcon icon={faGithub} className="icon" />
+                        <FontAwesomeIcon icon={faFacebook} className="icon" />
                     </div>
                     <span>or use your email for registered</span>
-                    <input type="text" name="Name" placeholder="Name" />
-                    <input type="email" name="Email" placeholder="Email" />
-                    <input type="password" name="Password" placeholder="Password" />
-                    <input type="password" name="Confirm_Password" placeholder="Confirm Password" />
-                    <input type="Date" name="Date_Of_Birth" />
-                    <select name="Gender" id="">
+                    <input type="text" name="username" placeholder='Username' value={formData.username} onChange={dataChange} />
+                    <input type="email" name="email" placeholder='email' value={formData.email} onChange={dataChange} />
+                    <input type="password" name="password" placeholder='password' value={formData.password} onChange={dataChange} />
+                    <input type="password" name="c_password" placeholder='Confirm Password' />
+                    <input type="text" name='firstname' placeholder='First Name' value={formData.firstname} onChange={dataChange} />
+                    <input type="text" name='lastname' placeholder='Last Name' value={formData.lastname} onChange={dataChange} />
+                    <input type="date" name="date_of_birth" value={formData.date_of_birth} onChange={dataChange} />
+                    <select name="gender" value={formData.gender} onChange={dataChange}>
                         <option value="">Select Gender</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
@@ -35,19 +70,19 @@ function LoginSignup() {
                     {/*<input type="text" name="Address" placeholder="Your address" />
                     <input type="text" name="Phone_Number" placeholder="Phone Number" />
                     <input type="text" name="Additional_Information" placeholder="More Information" />*/}
-                    <button>Sign up</button>
+                    <button type='submit'>Sign up</button>
                 </form>
             </div>
             <div className='Form-container sign-in'>
                 <form>
                     <h1>Sign In</h1>
                     <div className="Social-icons">
-                        <FontAwesomeIcon icon={faGoogle} className="icon"/>
-                        <FontAwesomeIcon icon={faGithub} className="icon"/>
-                        <FontAwesomeIcon icon={faFacebook} className="icon"/>
+                        <FontAwesomeIcon icon={faGoogle} className="icon" />
+                        <FontAwesomeIcon icon={faGithub} className="icon" />
+                        <FontAwesomeIcon icon={faFacebook} className="icon" />
                     </div>
                     <span>or use your email password for sign in</span>
-                    <input type="email" name="Email" placeholder="Email" />
+                    <input type="email" name="email" placeholder="Email" />
                     <input type="password" name="Password" placeholder="Password" />
                     <Anchor href="#">Forgot Your Password ?</Anchor>
                     <button>Sign In</button>

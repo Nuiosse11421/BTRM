13002429483032d8c3e6558a49c8573757beaef7
Nuiosse11421 <notice.gameacc@gmail.com>
2024-02-23 20:08:58 +0700

    update server

--- server/index.mjs
+++ server/index.mjs
@@ -1,14 +1,27 @@
 import express from 'express'
-import db from './db.mjs'
+import sequelize from './db.mjs'
 import cors from 'cors'
 import registerController from './routes/registerController.mjs'
+
+
 const app = express()
+
+app.use(express.json())
 app.use(cors())
 
-
-
+app.get('/',(req,res)=>{
+    res.send('Hello from express backend')
+    console.log("get/ complete")
+})
 //router
 app.use('/register', registerController)
-app.listen(8000,()=>{
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,async()=>{
     console.log('listening on http://localhost:8000')
+    try {
+        await sequelize.sync();
+        console.log('-----------Database synchronized----------------------');
+      } catch (error) {
+        console.error('Error synchronizing database:', error.message);
+      }
 })
--- server/routes/registerController.mjs
+++ server/routes/registerController.mjs
@@ -1,14 +1,32 @@
 // use /reigster/api/createUser to use registered form
 import express from 'express'
-const routers = express()
+import User from '../models/userModel.mjs'
+import Profile from '../models/profileModel.mjs'
+const router = express.Router();
 
+router.post("/api/createUser", async (req, res) => {
+  const { username, email, password, name, lastname, age, nationality, gender, date_of_birth } = req.body;
+  try {
+    // Create user
+    const user = await User.create({ username, email, password });
 
-routers.post("/api/createUser", async(req,res)=>{
-    try{
+    // Create profile
+    const profile = await Profile.create({
+      name,
+      lastname,
+      age,
+      nationality,
+      gender,
+      date_of_birth,
+      UserId: user.id 
+    });
 
-    }catch(err){
-        console.error(err.message)
-    }
-})
+    
+    res.status(201).json({ message: "User registered successfully", user, profile });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Internal server error",message:err.message }); 
+  }
+});
 
-export default routers
+export default router

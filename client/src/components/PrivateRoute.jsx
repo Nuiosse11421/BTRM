f15cb5d59d819a46f4ad9b44dae4df5877824146
Nuiosse11421 <notice.gameacc@gmail.com>
2024-02-26 23:25:15 +0700

    add authen login

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,35 +1,12 @@
-import React from "react"
-import { Route, Navigate } from 'react-router-dom'
-import {jwtDecode} from "jwt-decode"
+import React from "react";
+import {Route,Navigate} from 'react-router-dom'
 
-
-const isAuthenticated = () => {
-    const token = localStorage.getItem('token')
-    if(token){
-        try{
-            const decodedToken = jwtDecode(token)
-            const currentTime = Date.now()/1000
-            if (decodedToken.exp<currentTime){
-                localStorage.removeItem('token')
-                return false
-            }
-            return true
-        }catch(err){
-            return false
-        }
-    }
-    else{
-        return false
-    }
+const PrivateRoute = ({element,isAuthenticated, ...rest})=>{
+    return isAuthenticated()?(
+        <Route {...rest} element={element}/>
+    ) : (
+        <Navigate to="/login"/>
+    )
 }
-
-const PrivateRoute = ({ element, ...rest }) => {
-    return (
-      <Route
-        {...rest}
-        element={isAuthenticated() ? element : <Navigate to="/login" />}
-      />
-    );
-  }
 
 export default PrivateRoute

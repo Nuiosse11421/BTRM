c5712036cd032998a98f414e5752db630a31bd97
Nuiosse11421 <notice.gameacc@gmail.com>
2024-02-26 23:22:59 +0700

    update usermodel delete usernmae

--- server/models/userModel.mjs
+++ server/models/userModel.mjs
@@ -2,11 +2,6 @@ import { DataTypes } from "sequelize";
 import sequelize from '../db.mjs';
 
 const User = sequelize.define('User', {
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,

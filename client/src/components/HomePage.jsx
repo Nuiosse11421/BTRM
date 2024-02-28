d450c80cccae62a0853994a5a9dab164fed9fa2c
Nuiosse11421 <notice.gameacc@gmail.com>
2024-02-28 22:30:49 +0700

    update HomePage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NavBar from './NavBar';
 function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <Link to="/form">
-        <button>Go to Form Page</button>
-      </Link>
-      <Link to="/result">
-        <button>Go to Result Page</button>
-      </Link>
-      <Link to="/login">Login</Link>
+      <NavBar />
+      <div>
+        <h1>Home Page</h1>
+        <Link to="/form">
+          <button>Go to Form Page</button>
+        </Link>
+        <Link to="/result">
+          <button>Go to Result Page</button>
+        </Link>
+        <Link to="/login">Login</Link>
+      </div>
     </div>
   );
 }

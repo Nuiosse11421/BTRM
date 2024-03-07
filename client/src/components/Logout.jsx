088d4178c81551be69d5c0be62a499ccb61311ae
Nuiosse11421 <notice.gameacc@gmail.com>
2024-03-07 16:48:25 +0700

    update logout

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const Logout = () => {
   const navigate = useNavigate();
 

58beef75c7119c86fd949a27ee4e9132f92fd7cd
Nuiosse11421 <notice.gameacc@gmail.com>
2024-03-09 20:51:07 +0700

    Change data base to mongodb

--- server/db.mjs
+++ server/db.mjs
@@ -1,6 +1,11 @@
-import { Sequelize } from "sequelize"
-const sequelize = new Sequelize('IRT','postgres','nnu1123',{
-    host:"localhost",
-    dialect: 'postgres'
-})
-export default sequelize
+import mongoose from 'mongoose'
+const connectDB = async()=>{
+    try{
+        await mongoose.connect('mongodb+srv://notice:lmo1123@btrmserver.jz9kdr4.mongodb.net/BTRM?retryWrites=true&w=majority')
+        console.log('MongoDB connected');
+    }catch(err){
+        console.error(err);
+    }
+}
+
+export default connectDB

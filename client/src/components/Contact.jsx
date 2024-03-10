315f66437b30980521821679f8906f079a828a9c
PhawatThalalai <world.2545@gmail.com>
2024-03-10 17:20:55 +0700

    Merge branch 'front-end' of https://github.com/Nuiosse11421/BTRM into front-end

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // ContactManagement.jsx
 import React, { useState } from 'react';
 import './css/ContactManager.css'; // Import CSS file
@@ -101,11 +102,27 @@ const ContactManagement = () => {
         setContacts([...contacts, newContact]);
     };
 
+=======
+// ContactManager.jsx
+import React, { useState } from 'react';
+import './CSS/ContactManager.css'; // Import CSS file
+import ContactList from './ContactList'; // Import ContactList component
+import NavBar from './NavBar';
+const ContactManager = () => {
+    const [contacts, setContacts] = useState([]);
+
+    const addContact = (name) => {
+        const newContact = { name };
+        setContacts([...contacts, newContact]);
+    };
+
+>>>>>>> d7d5da2bb4a427601a79a82436fe4d2c018e5217
     const removeContact = (index) => {
         const updatedContacts = contacts.filter((_, i) => i !== index);
         setContacts(updatedContacts);
     };
 
+<<<<<<< HEAD
     const searchContact = (name) => {
         const searched = contacts.filter(contact => contact.name.toLowerCase().includes(name.toLowerCase()));
         setSearchedContacts(searched);
@@ -163,6 +180,32 @@ const ContactManagement = () => {
                                 </div>
                             </div>
                         ))}
+=======
+    return (
+        <>
+
+            <div className="app">
+                <NavBar />
+                <div className="app-container">
+                    <h1>Contact Management</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores amet incidunt, 
+                        mollitia beatae ea eaque quibusdam numquam repellat enim! Reprehenderit?</p>
+                    <div className="add-contact">
+                        <h2>Add Contact</h2>
+                        <form onSubmit={(e) => {
+                            e.preventDefault();
+                            const name = e.target.elements.name.value;
+                            addContact(name);
+                            e.target.reset();
+                        }}>
+                            <input type="text" name="name" placeholder="Enter Name or ID" />
+                            <button type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div className="contact-list">
+                        <h2>Contact List</h2>
+                        <ContactList contacts={contacts} removeContact={removeContact} />
+>>>>>>> d7d5da2bb4a427601a79a82436fe4d2c018e5217
                     </div>
                 </div>
             </div>
@@ -170,4 +213,8 @@ const ContactManagement = () => {
     );
 };
 
+<<<<<<< HEAD
 export default ContactManagement;
+=======
+export default ContactManager;
+>>>>>>> d7d5da2bb4a427601a79a82436fe4d2c018e5217
--- client/src/components/ContactList.jsx
+++ client/src/components/ContactList.jsx
@@ -1,18 +1,30 @@
 // ContactList.jsx
 import React from 'react';
+<<<<<<< HEAD
 import './css/ContactList.css';
 
 const ContactList = ({ contacts, removeContact }) => {
     return (
         <div className="contact-list-details">
+=======
+import './CSS/ContactList.css'; // Import CSS file
+
+const ContactList = ({ contacts, removeContact }) => {
+    return (
+        <div className="contact-list">
+>>>>>>> d7d5da2bb4a427601a79a82436fe4d2c018e5217
             {contacts.map((contact, index) => (
                 <div key={index} className="card">
                     <div className="card-body">
                         <h5 className="card-title">{contact.name}</h5>
+<<<<<<< HEAD
                         <p className="card-email">{contact.email}</p>
                         <div className="card-actions">
                             <button className="btn-custom" onClick={() => removeContact(index)}>Remove</button>
                         </div>
+=======
+                        <button className="btn btn-danger" onClick={() => removeContact(index)}>Remove</button>
+>>>>>>> d7d5da2bb4a427601a79a82436fe4d2c018e5217
                     </div>
                 </div>
             ))}

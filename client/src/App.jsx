f9a589f7272444b9de056b9a0759ee44913a3076
Nuiosse11421 <notice.gameacc@gmail.com>
2024-03-10 21:54:25 +0700

    update route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './LoginSignup'
 import Home from './components/HomePage'
-import FormComponent from './page/formtest/formtest.component'
-import FormTest from './pages/FormTest/'
+import FormComponent from './page/FormComponent'
+import FormTest from './page/formtest'
 import ContactManagement from './components/Contact' 
 
 const isAuthenticated = () => {
@@ -13,14 +13,15 @@ const isAuthenticated = () => {
   return !!token
 }
 const App = () => {
+  const loggendIn = isAuthenticated()
   return (
     <Router>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/' element={<Home/>}/>
-        <Route path='/form' element={<FormComponent/>}/>
-        <Route path='/formtest' element={<FormTest/>}/>
-        <Route path='/contact' element={<ContactManagement/>}/>
+        <Route path='/form' element={loggendIn ? <FormComponent /> : <Navigate to="/login" replace/>}/>
+        <Route path='/formtest' element={loggendIn ? <FormTest /> : <Navigate to="/login" replace/>}/>
+        <Route path='/contact' element={loggendIn ? <ContactManagement /> : <Navigate to="/login" replace/>}/>
         {/* Add more routes here */}
       </Routes>
     </Router>

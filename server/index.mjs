85e1ec01d1517d7457bb468155ba2de30ecfd40e
Nuiosse11421 <notice.gameacc@gmail.com>
2024-03-12 22:06:36 +0700

    update node_module

--- server/index.mjs
+++ server/index.mjs
@@ -1,24 +1,34 @@
 import express from 'express'
 import db from './db.mjs'
+import session from 'express-session'
 import cors from 'cors'
 import registerController from './routes/registerController.mjs'
 import loginCon from './routes/loginController.mjs'
 import formController from './routes/formController.mjs'
-
+import hasAuthController from './routes/hasAuthController.mjs'
+import authMiddleware from './routes/authUserController.mjs'
 
 const app = express()
 
+
 app.use(express.json())
+app.use(express.urlencoded())
 app.use(cors())
+app.use(session({
+    secret:'secret',
+    resave: true,
+    saveUninitialized:true,
+}))
 db()
+
 app.get('/',(req,res)=>{
     res.send('Hello from express backend')
     console.log("get/ complete")
 })
 //router
-app.use('/register', registerController)
-app.use('/login', loginCon)
-app.use('/form', formController)
+app.use('/api' ,registerController)
+app.use('/api' ,loginCon)
+app.use('/api',formController)
 
 
 

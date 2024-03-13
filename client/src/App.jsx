a915ad12215040a221efdf18674d95c62eccce8a
Nuiosse11421 <notice.gameacc@gmail.com>
2024-03-14 01:38:15 +0700

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable react/jsx-no-undef */
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './LoginSignup'
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import LoginSignup from './page/LoginSignup';
 import Home from './page/HomePage'
+import HomeTest from './page/hometest'
 import FormComponent from './page/FormComponent'
 import FormTest from './page/formtest'
 import ContactManagement from './components/Contact' 
+import './App.css'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/login' element={<Login />} />
         <Route path='/' element={<Home/>}/>
-        <Route path='/form' element={ <FormComponent />}/>
-        <Route path='/formtest' element={ <FormTest />}/>
-        <Route path='/contact' element={ <ContactManagement />}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+        <Route path='/form' element={ <FormTest />}/>
       </Routes>
     </Router>
   );

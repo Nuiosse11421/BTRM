5ddb0a0a223d9577f770c25eb832e1a870fa572a
nuiosse11421 <notice.gameacc@gmail.com>
2024-03-26 23:22:27 +0700

    add role card into Homepage.jsx

--- client/src/page/HomePage.jsx
+++ client/src/page/HomePage.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import NavBar from '../components/NavBar';
+import RoleCard from "../components/RoleCard";
 function HomePage() {
 
   return (
     <div>
       <NavBar />
       <div>homepage</div>
+      <RoleCard/>
     </div>
   );
 }

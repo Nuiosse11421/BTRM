bb97b7582489fdf25f2648c7d6376ec3a5622790
nuiosse11421 <notice.gameacc@gmail.com>
2024-03-27 22:44:05 +0700

    createfile: Profile Team .jsx in page Folder

---
+++ client/src/page/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+const Profile = () => {
+    return (
+        <div>
+            <NavBar />
+            Profile
+        </div>
+    )
+}
+
+export default Profile

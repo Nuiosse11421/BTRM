58f8728ee3fcae1a73ebc6c0385996696be5562b
nuiosse11421 <notice.gameacc@gmail.com>
2024-03-31 23:51:15 +0700

    update: search contact add contact and remove contact

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,121 +1,101 @@
 // ContactManagement.jsx
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './css/ContactManager.css'; // Import CSS file
 import NavBar from './NavBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useGetUserID } from '../hook/useGetUserID';
 
 const ContactManagement = () => {
-    const [contacts, setContacts] = useState([
-        {
-            name: "John Doe",
-            position: "Manager",
-            email: "john.doe@example.com",
-            
-        },
-        {
-            name: "Jane Smith",
-            position: "Sales Representative",
-            email: "jane.smith@example.com",
-            
-        },
-        {
-            name: "Alice Johnson",
-            position: "Developer",
-            email: "alice.johnson@example.com",
-            
-        },
-        {
-            name: "Bob Johnson",
-            position: "Data Analyst",
-            email: "bob.johnson@example.com",
-            
-        },
-        {
-            name: "Sarah Lee",
-            position: "Data Scientist",
-            email: "sarah.lee@example.com",
-            
-        },
-        {
-            name: "Michael Wang",
-            position: "Database Administrator",
-            email: "michael.wang@example.com",
-            
-        },
-        {
-            name: "Emily Chen",
-            position: "Data Engineer",
-            email: "emily.chen@example.com",
-            
-        },
-        {
-            name: "Daniel Kim",
-            position: "Machine Learning Engineer",
-            email: "daniel.kim@example.com",
-            
-        },
-        {
-            name: "Jessica Wong",
-            position: "Business Intelligence Analyst",
-            email: "jessica.wong@example.com",
-            
-        },
-        {
-            name: "Alex Zhang",
-            position: "Data Architect",
-            email: "alex.zhang@example.com",
-            
-        },
-        {
-            name: "Sophia Liu",
-            position: "Data Quality Analyst",
-            email: "sophia.liu@example.com",
-            
-        },
-        {
-            name: "Ryan Chen",
-            position: "Big Data Engineer",
-            email: "ryan.chen@example.com",
-            
-        },
-        {
-            name: "Grace Wu",
-            position: "Data Visualization Specialist",
-            email: "grace.wu@example.com",
-            
-        },
-        {
-            name: "Phawat Thalalai",
-            position: "Frontend Developer",
-            email: "Phawat.th@example.com",
-            
-        }
-    ]);
 
     const [searchedContacts, setSearchedContacts] = useState([]);
+    const [contactList, setContactList] = useState([])
+    const userID = useGetUserID()
+    const handleFormSumbit = async (e) => {
+        e.preventDefault()
+        const form = e.target
+        const formData = new FormData(form)
+        const name = formData.get('name')
+        try {
+            const response = await axios.get(`http://localhost:8000/api/search-profile`, {
+                params: {
+                    search: name,
+                    userID: userID
+                }
+            })
+            setSearchedContacts(response.data.contacts)
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
-    const addContact = (name) => {
-        const newContact = { name };
-        setContacts([...contacts, newContact]);
+    const handleAddContact = async (contactEmail) => {
+        try {
+            const contactUser = await axios.get(`http://localhost:8000/api/search-profile`, {
+                params: {
+                    search: contactEmail,
+                    userID: userID
+                }
+            });
+
+            if (!contactUser.data.contacts.length) {
+                alert('Contact user not found');
+                return;
+            }
+
+            if (userID === contactUser.data.contacts[0]._id) {
+                alert('You cannot add yourself as a contact');
+                return;
+            }
+
+            const response = await axios.post('http://localhost:8000/api/add-contact', {
+                userID: userID,
+                contactEmail: contactEmail
+            });
+
+            alert(response.data.message);
+        } catch (err) {
+            alert('Maybe you have already this Contact');
+        }
     };
+    const formatDate = (data) => {
+        const options = { year: 'numeric', month: 'numeric', day: 'numeric' }
+        return new Date(data).toLocaleDateString(undefined, options)
+    }
+    const handleRemoveContact = async (contactEmail) => {
+        try {
+            await axios.delete('http://localhost:8000/api/delete-contact', {
+                data: {
+                    userID: userID,
+                    contactEmail: contactEmail
+                }
+            })
+            alert('Remove Contact Successful')
+            const updatedContactList = contactList.filter(contact => contact.email !== contactEmail);
+            setContactList(updatedContactList);
+        } catch (err) {
 
-    const removeContact = (index) => {
-        const updatedContacts = contacts.filter((_, i) => i !== index);
-        setContacts(updatedContacts);
-    };
+        }
+    }
 
-    const searchContact = (name) => {
-        const searched = contacts.filter(contact => contact.name.toLowerCase().includes(name.toLowerCase()));
-        setSearchedContacts(searched);
-    };
 
-    const handleInputChange = (e) => {
-        const name = e.target.value;
-        searchContact(name);
-    };
-
+    const fetchContactList = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/contact-list', {
+                params: {
+                    userID: userID
+                }
+            })
+            setContactList(response.data.contacts)
+        }
+        catch (err) {
+        }
+    }
+    useEffect(() => {
+        fetchContactList()
+    }, [userID])
     return (
         <>
             <NavBar />
@@ -124,17 +104,11 @@ const ContactManagement = () => {
                     mollitia beatae ea eaque quibusdam numquam repellat enim! Reprehenderit?</p>
                 <div className="add-contact">
                     <h2>Add Contact</h2>
-                    <form onSubmit={(e) => {
-                        e.preventDefault();
-                        const name = e.target.elements.name.value;
-                        searchContact(name);
-                        e.target.reset();
-                    }}>
+                    <form onSubmit={handleFormSumbit}>
                         <input
                             type="text"
                             name="name"
-                            placeholder="Enter Name or ID"
-                            onChange={handleInputChange}
+                            placeholder="Enter Name or Email"
                         />
                         <button type="submit">
                             <FontAwesomeIcon icon={faSearch} />
@@ -152,18 +126,37 @@ const ContactManagement = () => {
                                     </div>
                                     <div className="contact-details">
                                         <ul className='contact-details-group'>
-                                            <li><strong>Name:</strong> {contact.name}</li>
-                                            <li><strong>Position:</strong> {contact.position}</li>
-                                            <li><strong>Email:</strong> {contact.email}</li>
+                                            <li><strong>Name: </strong> {contact.firstname + ' ' + contact.lastname}</li>
+                                            <li><strong>Position: </strong> {contact.position}</li>
+                                            <li><strong>Roles Score: </strong></li>
+                                            <li><strong>Email: </strong> {contact.email}</li>
+                                            <li><strong>Gender: </strong>{contact.gender}</li>
+                                            <li><strong>Birthday: </strong>{formatDate(contact.date_of_birth)}</li>
                                         </ul>
-                                    </div>
-                                    <div className="card-actions">
-                                        <button className="btn-custom" onClick={() => removeContact(index)}>Add</button>
+                                        <div className="card-actions">
+                                            <button className="btn-custom" onClick={() => handleAddContact(contact.email)}>Add</button>
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
                         ))}
                     </div>
+                </div>
+            </div>
+            <div className='list-contacts'>
+                <h3>Contact List</h3>
+                <div>
+                    {contactList.map((contact, index) => (
+                        <div className='contact-list-card' key={index}>
+                            <ul>
+                                <li><strong>Name: </strong>{contact.firstname + ' ' + contact.lastname}</li>
+                                <li><strong>Email: </strong>{contact.email}</li>
+                                <li><strong>Gender: </strong>{contact.gender}</li>
+                            </ul>
+                            <button className='remove-contact' onClick={() => handleRemoveContact(contact.email)}>Remove</button>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>

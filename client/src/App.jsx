0e5d70719e4c9efb08a7adede751668983115c57
nuiosse11421 <notice.gameacc@gmail.com>
2024-04-01 22:57:10 +0700

    update: update some line code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/jsx-no-undef */
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './page/LoginSignup'
 import Home from './page/HomePage'
 import FormTest from './page/formtest'
 import ContactManagement from './components/Contact'
-import Profile from './page/Profile';
 import Team from './page/Team';
 import RoleChart from './components/RoleChart';
 import History from './page/History';
@@ -24,7 +23,6 @@ const App = () => {
         <Route path='/history' element={ <History />}/>
         <Route path='/contact' element={ <ContactManagement />}/>
         <Route path='/team' element={ <Team />}/>
-        <Route path='/profile' element={ <Profile />}/>
 
       </Routes>
     </Router>

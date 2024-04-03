44dd3da333d894f7b189bb95578e26dd8cfee0dd
nuiosse11421 <notice.gameacc@gmail.com>
2024-04-03 22:30:22 +0700

    update: update Router in history

--- server/routes/historyController.mjs
+++ server/routes/historyController.mjs
@@ -11,11 +11,13 @@ router.get('/history', async (req, res) => {
         if (!userID) {
             return res.status(404).json({ message: 'user not Found' })
         }
+        const HisRole = await RoleHistory.findOne({_id:userID})
         const HisAns = await AnswerHistory.findOne({_id:userID})
+        const roleHistory = HisRole.roles
+        roleHistory.sort((a,b)=> new Date(b.timestamp) - new Date(a.timestamp))
         const answerHistory = HisAns.history
         answerHistory.sort((a,b)=> new Date(b.timestamp)- new Date(a.timestamp))
-
-        res.json({ answerHistory })
+        res.json({ answerHistory ,roleHistory})
     } catch (err) {
     }
 })
